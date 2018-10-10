0d096a486c96411a42ff5ca457b7a10aab35076c
SHAJ <shakilahmedshaj@gmail.com>
2018-10-10 21:00:31 +0600

    Data Parser URL Done

---
+++ app/src/main/java/com/t3ch/shaj/easy_finder/NearByPlaces.java
@@ -0,0 +1,69 @@
+package com.t3ch.shaj.easy_finder;
+
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Shakil Ahmed Shaj on 10-Oct-18.
+ */
+public class NearByPlaces extends AsyncTask<Object, String, String> {
+    private String googleplaceData, url;
+    private GoogleMap mMap;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        mMap = (GoogleMap) objects[0];
+        url = (String) objects[1];
+
+        downloadURL downloadUrl = new downloadURL();
+        try {
+            googleplaceData = downloadUrl.ReadTheURL(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googleplaceData;
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        List<HashMap<String, String>> nearByPlacesList = null;
+        DataParser dataParser = new DataParser();
+        nearByPlacesList = dataParser.parse(s);
+
+        DisplayNearbyPlaces(nearByPlacesList);
+    }
+
+
+    private void DisplayNearbyPlaces(List<HashMap<String, String>> nearByPlacesList) {
+        for (int i = 0; i < nearByPlacesList.size(); i++) {
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            HashMap<String, String> googleNearbyPlace = nearByPlacesList.get(i);
+            String nameOfPlace = googleNearbyPlace.get("place_name");
+            String vicinity = googleNearbyPlace.get("vicinity");
+            double lat = Double.parseDouble(googleNearbyPlace.get("lat"));
+            double lng = Double.parseDouble(googleNearbyPlace.get("lng"));
+
+
+            LatLng latLng = new LatLng(lat, lng);
+            markerOptions.position(latLng);
+            markerOptions.title(nameOfPlace + " : " + vicinity);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+            mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        }
+    }
+
+}
---
+++ app/src/main/java/com/t3ch/shaj/easy_finder/downloadURL.java
@@ -0,0 +1,48 @@
+package com.t3ch.shaj.easy_finder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Shakil Ahmed Shaj on 10-Oct-18.
+ */
+public class downloadURL {
+    public String ReadTheURL(String placeURL) throws IOException {
+        String Data = "";
+        InputStream inputStream = null;
+        HttpURLConnection httpURLConnection = null;
+
+        try {
+            URL url = new URL(placeURL);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+
+            inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String line = "";
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+
+            Data = stringBuffer.toString();
+            bufferedReader.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            inputStream.close();
+            httpURLConnection.disconnect();
+        }
+
+        return Data;
+    }
+}
